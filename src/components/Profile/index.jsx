9f4fc1496e113d026334eb8b60e405920c04ad68
Ryo Kihara <kihara@gizumo-inc.jp>
2023-07-03 01:09:03 +0900

    add: Profile

---
+++ src/components/Profile/index.jsx
@@ -0,0 +1,15 @@
+import './index.css'
+
+export const Profile = () => {
+  const imgUrl = 'https://i.imgur.com/7vQD0fPs.jpg';
+  const name = 'John'
+  const getBirthDay = () => {
+    return new Date().toLocaleDateString()
+  }
+
+  return (
+    <img src="imgUrl" className="avatar">
+    <p className="sentence">I am John</p>
+    <p className="sentence">BirthDay: </p>
+  )
+}
