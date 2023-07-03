cabf9ebe8dc645fdbc1d273db8bb2c2f8f6366eb
Ryo Kihara <kihara@gizumo-inc.jp>
2023-07-03 10:40:16 +0900

    add: Country

---
+++ src/components/Country/index.jsx
@@ -0,0 +1,8 @@
+export const Country = () => {
+  return (
+    <div>
+      <img src={info.imgUrl} width={width} height={height} />
+      <p className="country-name">{info.name}</p>
+    </div>
+  )
+}
