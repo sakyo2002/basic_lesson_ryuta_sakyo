61fa15b72c3db0293219e1d1dd468127253f1617
Ryo Kihara <kihara@gizumo-inc.jp>
2023-07-03 11:38:18 +0900

    add: Card

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,16 @@
+import PropTypes from 'prop-types'
+import './index.css'
+
+export const Card = () => {
+  return (
+    <div className="card"></div>
+  )
+}
+
+Card.propTypes = {
+  children: PropTypes.node
+}
+
+Card.defaultProps = {
+  children: null
+}
