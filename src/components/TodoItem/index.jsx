5708ceb79c433537d69475461d68f2cd9ae23e7a
Ryo Kihara <kihara@gizumo-inc.jp>
2023-07-12 12:36:43 +0900

    chore: スペルミス

--- src/components/TodoItem/index.jsx
+++ src/components/TodoItem/index.jsx
@@ -1,12 +1,12 @@
 import PropTypes from 'prop-types'
 import styles from './index.module.css'
 
-export const TodoItem = ({ name, isToday, isCompoleted }) => {
+export const TodoItem = ({ name, isToday, isCompleted }) => {
   return <li className={styles.name}>{name}</li>
 }
 
 TodoItem.propTypes = {
   name: PropTypes.string.isRequired,
   isToday: PropTypes.bool.isRequired,
-  isCompoleted: PropTypes.bool.isRequired,
+  isCompleted: PropTypes.bool.isRequired,
 }
