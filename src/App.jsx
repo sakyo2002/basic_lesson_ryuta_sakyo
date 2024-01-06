c7ce61f4dc1a7f8414af0d9250b71bf57de8def8
sakyo2002 <sakyo.ryuta@gmail.com>
2024-01-06 21:43:31 +0900

    練習問題1

--- src/App.jsx
+++ src/App.jsx
@@ -1,2 +1,13 @@
 import reactLogo from './assets/react.svg'
 import styles from './App.module.css'
+
+export const App = () => {
+  return (
+    <div>
+      <img src={reactLogo} className={styles['react-logo']} />
+      <h1>Hello React!</h1>
+
+      {/* 以下に練習問題の回答を記述してください */}
+    </div>
+  )
+};
