0a95d7c2864387f92e5ced00699495da7a0edc18
sakyo2002 <sakyo.ryuta@gmail.com>
2024-02-13 20:27:04 +0900

    練習問題３を訂正

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -10,7 +10,7 @@ export const Profile = () => {
   return (
     <>
       <img src={imgUrl} className={styles.avatar}/>
-      <p className={styles.sentence}>I am John</p>
+      <p className={styles.sentence}>I am {name}</p>
       <p className={styles.sentence}>BirthDay:{getBirthDay()}</p>
     </>
   )
